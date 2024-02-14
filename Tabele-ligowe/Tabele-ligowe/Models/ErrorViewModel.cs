50d2289e0f45ee82e51c3227eaa7c4b4ed54ec73
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-15 00:58:11 +0100

    initial commit

---
+++ Tabele-ligowe/Tabele-ligowe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tabele_ligowe.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
