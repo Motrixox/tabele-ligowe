699e47a594daad2aa04ad1e9238c3392d39fdf9c
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-15 22:01:25 +0100

    added InMemory database connection and dependency injection for services

--- Tabele-ligowe/Tabele-ligowe/Program.cs
+++ Tabele-ligowe/Tabele-ligowe/Program.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Tabele_ligowe.Data;
+using Tabele_ligowe.Models;
+using Tabele_ligowe.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
+	//options.UseSqlServer(connectionString));
+	options.UseInMemoryDatabase("Scoreboard"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 	.AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IRepositoryService<Team>, RepositoryService<Team>>();
+builder.Services.AddScoped<IRepositoryService<Match>, RepositoryService<Match>>();
+builder.Services.AddSingleton<ScoreBoardService, ScoreBoardService>();
 
 var app = builder.Build();
 
