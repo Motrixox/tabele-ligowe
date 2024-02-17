df56a1c47bdcb783c1698d5f0182313d96572969
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-18 00:14:22 +0100

    updated scoreboard viewmodel

--- Tabele-ligowe/Tabele-ligowe/Models/ScoreboardViewModel.cs
+++ Tabele-ligowe/Tabele-ligowe/Models/ScoreboardViewModel.cs
@@ -4,11 +4,18 @@
     {
         public List<MatchViewModel> Matches { get; set; }
         public List<TeamViewModel> Teams { get; set; }
+        public Guid SelectedLeagueId { get; set; }
+        public Guid SelectedSeasonId { get; set; }
+        public List<League> Leagues { get; set; }
+        public List<Season> Seasons { get; set; }
+        public int NumOfRounds { get; set; }
 
         public ScoreboardViewModel()
         {
             Matches = new List<MatchViewModel>();
             Teams = new List<TeamViewModel>();
+            Leagues = new List<League>();
+            Seasons = new List<Season>();
         }
     }
 }
