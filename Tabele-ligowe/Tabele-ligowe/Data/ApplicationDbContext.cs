726d74dcd444ce13f41a4961f5fe98f241ab163e
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-18 00:16:04 +0100

    updated database seeding with new leagues, seasons, teams & matches

--- Tabele-ligowe/Tabele-ligowe/Data/ApplicationDbContext.cs
+++ Tabele-ligowe/Tabele-ligowe/Data/ApplicationDbContext.cs
@@ -7,6 +7,8 @@ namespace Tabele_ligowe.Data
 {
 	public class ApplicationDbContext : IdentityDbContext
 	{
+		public DbSet<League> Leagues { get; set; }
+		public DbSet<Season> Seasons { get; set; }
 		public DbSet<Team> Teams { get; set; }
 		public DbSet<Match> Matches { get; set; }
 
@@ -35,7 +37,19 @@ namespace Tabele_ligowe.Data
 		{
             if (Teams.Count() > 0) return;
 
-            List<string> teams = new List<string> {
+
+            var league = new League { Id = Guid.NewGuid(), Name = "Liga Belgijska" };
+            var league2 = new League { Id = Guid.NewGuid(), Name = "Testowa liga" };
+
+            var season = new Season { Id = Guid.NewGuid(), Name = "2022/23", LeagueId = league.Id };
+            var season2 = new Season { Id = Guid.NewGuid(), Name = "2023/24", LeagueId = league.Id };
+            var season3 = new Season { Id = Guid.NewGuid(), Name = "2022/23", LeagueId = league2.Id };
+            var season4 = new Season { Id = Guid.NewGuid(), Name = "2023/24", LeagueId = league2.Id };
+
+            Leagues.AddRange(league, league2);
+            Seasons.AddRange(season, season2, season3, season4);
+
+            List<string> teamNames = new List<string> {
                 "Genk",
                 "Union Saint-Gilloise",
                 "Antwerp",
@@ -56,20 +70,46 @@ namespace Tabele_ligowe.Data
                 "Seraing United",
             };
 
-            foreach (var team in teams)
+            List<string> teamNames2 = new List<string> {
+                "test1",
+                "test2",
+                "test3",
+                "test4",
+            };
+
+            foreach (var teamName in teamNames)
             {
-                Teams.Add(new Team { Id = Guid.NewGuid(), Name = team });
+                var team = new Team { Id = Guid.NewGuid(), Name = teamName };
+                team.Seasons = new List<Season> { season, season2 };
+                Teams.Add(team);
+            }
+
+            foreach (var teamName in teamNames2)
+            {
+                var team = new Team { Id = Guid.NewGuid(), Name = teamName };
+                team.Seasons = new List<Season> { season3, season4 };
+                Teams.Add(team);
             }
 
             SaveChanges();
 
-            SeedLeagueRound(teams, 1);
-            SeedLeagueRound(teams, 2);
+            SeedLeagueRound(teamNames, 1, season.Id);
+            SeedLeagueRound(teamNames, 2, season.Id);
+
+            SeedLeagueRound(teamNames, 1, season2.Id);
+            SeedLeagueRound(teamNames, 2, season2.Id);
+
+            SeedLeagueRound(teamNames2, 1, season3.Id);
+            SeedLeagueRound(teamNames2, 2, season3.Id);
+            SeedLeagueRound(teamNames2, 3, season3.Id);
+
+            SeedLeagueRound(teamNames2, 1, season4.Id);
+            SeedLeagueRound(teamNames2, 2, season4.Id);
 
             SaveChanges();
         }
 
-        private void SeedLeagueRound(List<string> teams, int leagueRound)
+        private void SeedLeagueRound(List<string> teams, int leagueRound, Guid seasonId)
         {
             var teamsTemp = new Queue<string>(teams.Shuffle());
 
@@ -82,7 +122,8 @@ namespace Tabele_ligowe.Data
                     AwayTeamId = Teams.First(x => x.Name.Equals(teamsTemp.Dequeue())).Id,
                     HomeTeamGoals = Random.Shared.Next(0, 7),
                     AwayTeamGoals = Random.Shared.Next(0, 7),
-                    LeagueRound = leagueRound
+                    LeagueRound = leagueRound,
+                    SeasonId = seasonId
                 });
             }
         }
