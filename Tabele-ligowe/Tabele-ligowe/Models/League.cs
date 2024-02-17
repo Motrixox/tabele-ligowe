b99bef35dbed1dc2225430863924a8a2029afa0c
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-18 00:13:14 +0100

    added new database models for league and season

---
+++ Tabele-ligowe/Tabele-ligowe/Models/League.cs
@@ -0,0 +1,9 @@
+﻿namespace Tabele_ligowe.Models
+{
+    public class League : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Season> Seasons { get; set; }
+    }
+}
---
+++ Tabele-ligowe/Tabele-ligowe/Models/Season.cs
@@ -0,0 +1,14 @@
+﻿namespace Tabele_ligowe.Models
+{
+    public class Season : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid LeagueId { get; set; }
+        public League League { get; set; }
+
+        public ICollection<Team> Teams { get; set; }
+        public ICollection<Match> Matches { get; set; }
+    }
+}
