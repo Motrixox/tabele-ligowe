5cb57d574777fc236d41af5d76af19de5fdcc89d
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-02-18 22:44:29 +0100

    added favorite teams list

--- Tabele-ligowe/Tabele-ligowe/Controllers/HomeController.cs
+++ Tabele-ligowe/Tabele-ligowe/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Tabele_ligowe.Models;
+using Tabele_ligowe.Services;
 using Tabele_ligowe.ViewModels;
 
 namespace Tabele_ligowe.Controllers
@@ -7,15 +9,42 @@ namespace Tabele_ligowe.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IRepositoryService<UserFavoriteTeam> _userFavoriteTeamRepository;
+		private readonly IRepositoryService<Team> _teamRepository;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger,
+            IRepositoryService<UserFavoriteTeam> userFavoriteTeamRepository,
+            IRepositoryService<Team> teamRepository)
 		{
 			_logger = logger;
-		}
+			_userFavoriteTeamRepository = userFavoriteTeamRepository;
+            _teamRepository = teamRepository;
+
+        }
 
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		public IActionResult Favorites()
+		{
+            var username = User?.Identity?.Name;
+
+			var model = new List<FavoriteTeamViewModel>();
+
+            if (username != null)
+			{
+				var favoriteTeams = _userFavoriteTeamRepository.FindBy(x => x.Username.Equals(username)).ToList();
+
+				foreach (var favTeam in favoriteTeams)
+				{
+					var team = _teamRepository.GetSingle(favTeam.TeamId);
+					model.Add(new FavoriteTeamViewModel{ Name = team.Name });
+				}
+            }
+
+            return View(model);
 		}
 
 		public IActionResult Privacy()
